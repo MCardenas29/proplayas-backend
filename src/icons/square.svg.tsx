8d0f80e2fd1c36cb2c13e06e46d3cfb02288f51e
Madjesc <mauricio.291202@gmail.com>
2024-05-20 23:45:00 -0600

    fix: Fix coloring of svg

--- src/icons/square.svg.tsx
+++ src/icons/square.svg.tsx
@@ -2,7 +2,7 @@ import { Icon } from "./icon";
 
 export default function({ className = '', size = 24 }: Icon) {
     return (
-        <svg className={className} width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg stroke="currentColor" className={className} width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 3.6V20.4C21 20.5591 20.9368 20.7117 20.8243 20.8243C20.7117 20.9368 20.5591 21 20.4 21H3.6C3.44087 21 3.28826 20.9368 3.17574 20.8243C3.06321 20.7117 3 20.5591 3 20.4V3.6C3 3.44087 3.06321 3.28826 3.17574 3.17574C3.28826 3.06321 3.44087 3 3.6 3H20.4C20.5591 3 20.7117 3.06321 20.8243 3.17574C20.9368 3.28826 21 3.44087 21 3.6Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
     )
--- src/icons/xmark.svg.tsx
+++ src/icons/xmark.svg.tsx
@@ -2,7 +2,7 @@ import { Icon } from "./icon";
 
 export default function({ className = '', size = 24 }: Icon) {
     return (
-        <svg className={className} width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg stroke="currentColor" className={className} width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.75806 17.243L12.0011 12L17.2441 17.243M17.2441 6.757L12.0001 12L6.75806 6.757" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
     )
